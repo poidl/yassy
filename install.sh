fbdd093dd493cb9a2161f6ec3c88a41e42140335
Stefan <hoitaus@gmail.com>
2016-09-10 10:49:52 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-LV2DIR=~/.lv2/
-BACKUPDIR=~/programs/backup_lv2/
+LV2DIR=~/.lv2
+BACKUPDIR=~/programs/backup_lv2
 BUNDLE=yassy.lv2
 SONAME=libyassy.so
-DEST=$LV2DIR$BUNDLE
+DEST=$LV2DIR/$BUNDLE
+if [ ! -d "$LV2DIR" ]; then
+  mkdir -p $LV2DIR;
+fi
 if [ -d "$DEST" ]; then
     if [ -d "$BACKUPDIR/$BUNDLE" ]; then
         rm -r $BACKUPDIR/$BUNDLE
