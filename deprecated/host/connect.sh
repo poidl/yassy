6806cc68ff1af67d647c71a8abe293f475def02f
Stefan <hoitaus@gmail.com>
2016-09-17 12:34:48 +0800

    deprecate host

--- deprecated/host/connect.sh
+++ deprecated/host/connect.sh
@@ -2,6 +2,6 @@ killall yassyhost
 sleep 0.2
 ./target/debug/yassyhost &
 sleep 0.2
-jack_connect "yassyhost:midi_in"  "a2j:Keystation 49 [24] (capture): Keystation 49 MIDI 1"
+jack_connect "yassyhost:midi_in"  "a2j:VMini [20] (capture): VMini MIDI 1"
 jack_connect "yassyhost:audio_out" "system:playback_1"
 jack_connect "yassyhost:audio_out" "system:playback_2"
