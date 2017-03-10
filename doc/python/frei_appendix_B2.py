f761b9b36ee2e78c71cf8225f9db6ea14e72de0a
Stefan <hoitaus@gmail.com>
2017-03-10 14:20:15 +1100

    Frei appendix B2

--- doc/python/frei_appendix_B2.py
+++ doc/python/frei_appendix_B2.py
@@ -7,9 +7,11 @@
 # Sound Technology (ICST) Zurich University of the Arts.
 
 import numpy as np
+import scipy.signal as sig
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+# import bindings
 import utils
 
 # parameters
@@ -67,4 +69,47 @@ plt.axvline(20, color="g")
 plt.axvline(fs / 1000 - 20, color="r")
 plt.axvline(fs / 1000, color="r")
 
+fig.savefig('../figures/' + figname)
+
+fcomp = 21000
+rng = 1 + 2 * np.floor(0.5 * rlen * zpad * fcomp / fs - 0.5)
+xidx = np.arange(rng + 1)
+a = wspec[: int(rng + 1)]
+a = 1.0 / a
+ftune = 0.35
+f = xidx / (ftune + rlen * zpad)
+wgt = np.arange((rng + 1) / 2,  0, -1)
+wgt = 1 + wgt * wgt
+# This is different to Frei's Matlab version. See Matlab/Scipy docs for
+# differences in call signatures
+aa = a[::2]
+b = sig.remez(16, f, aa, wgt)
+[w, h] = sig.freqz(b, 1, rlen * zpad, 'whole')
+
+figname = 'frei_xy.svg'
+fig = plt.figure()
+plt.plot(fs * f / 1000, a)
+plt.plot(0.5 * fs * w / np.pi / 1000, abs(h))
+plt.xlim(0, fs / 1000)
+plt.ylim(0, max(abs(h)))
+plt.xlabel('Frequency in kHz')
+plt.title('Prefilter Magnitude Response')
+plt.grid()
+fig.savefig('../figures/' + figname)
+
+
+#  check by convolving prefilter and bandlimited impulse
+imp = g[:ppiv:pts]
+res = np.convolve(b, imp)
+res = np.concatenate([res, np.zeros(1000 - len(res))])
+wspec = np.sqrt(np.abs(utils.rdtpsd(res)))
+rng = round(1000 * 20000 / fs)
+xidx = np.arange(rng + 1)
+
+figname = 'frei_xyz.svg'
+fig = plt.figure()
+plt.plot(20 * xidx / rng, wspec[: int(rng + 1)] / wspec[0])
+plt.xlabel('Frequency in kHz')
+plt.title('Normalized Overall Magnitude Response')
+plt.grid()
 fig.savefig('../figures/' + figname)
