7292d7a23ef6861378bf207e5f9224ccbfec246f
Stefan <hoitaus@gmail.com>
2017-03-25 14:06:32 +1100

    client_split()

--- ui/src/yassyui.rs
+++ ui/src/yassyui.rs
@@ -7,11 +7,11 @@ use websocket::{Message, Sender, Receiver};
 
 use websocket::server::request::Request;
 use websocket::client;
-// use websocket::message::Type;
 use websocket::header::WebSocketProtocol;
 use websocket::stream::WebSocketStream;
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 use rustc_serialize::json;
 
@@ -57,10 +57,12 @@ impl yassyui {
         };
         ui
     }
-    // Three threads are spawned:
 
-    // MAIN THREAD: port_event()
-    // Connecting channel:     hostin
+    // port_event() is part of the "main thread"
+    // Each UI instance has a 
+    // Two threads are spawned:
+
+    // Connecting channel: 
     // THREAD 1:    param_as_message_to_sendloop()
     //           
     //    Host----------->UI-------------------------------------->Browser
@@ -76,61 +78,16 @@ impl yassyui {
         let result = tcplistener.accept();
         match result {
             Ok(s) => {
-                
-                let tcpstream = s.0;
-                let wsstream = WebSocketStream::Tcp(tcpstream);
-                pub struct Connection<R: Read, W: Write>(R, W);
-                let connection = Connection(wsstream.try_clone().unwrap(),
-                                            wsstream.try_clone().unwrap());
-
-                // port_event() -> param_as_message_to_sendloop() -> send_loop() ->
-                // browser.
-                // browser -> receive_loop() -> write_function()
-                // browser -> receive_loop() -> send_loop() (for "Close" and "Ping")
-                // send parameter values from lv2::LV2UIDescriptor.port_event()
-                // to param_as_message_to_sendloop() to convert it to a message
+                let (mut sender, mut receiver) = client_split(s.0);
+ 
                 let (tx_hostin, rx_hostin) = mpsc::channel();
                 self.sender = tx_hostin;
 
-                // let (tx_wsout, rx_wsout) = mpsc::channel();
-                // there are 2 producers (transmitters) for the send_loop consumer
-                // (receiver). One forwards parameter values which have been translated
-                // to a Message, one redirects "Close" and "Ping" signals received
-                // from the browser back to the browser (through the send_loop).
-                // let tx_echo_wsout = tx_wsout.clone();
-
-
-                let request = Request::read(connection.0, connection.1).unwrap();
-                let headers = request.headers.clone(); // Keep the headers so we can check them
-
-                request.validate().unwrap(); // Validate the request
-
-                let mut response = request.accept(); // Form a response
-
-                if let Some(&WebSocketProtocol(ref protocols)) = headers.get() {
-                    if protocols.contains(&("rust-websocket".to_string())) {
-                        // We have a protocol we want to use
-                        response.headers
-                            .set(WebSocketProtocol(vec!["rust-websocket".to_string()]));
-                    }
-                }
-                let mut client = response.send().unwrap(); // Send the response
-
-                let ip = client.get_mut_sender()
-                    .get_mut()
-                    .peer_addr()
-                    .unwrap();
-
-                println!("Connection from {}", ip);
-
-                let message: Message = Message::text("Hello".to_string());
-                client.send_message(&message).unwrap();
-
-                let (mut sender, mut receiver) = client.split();
 
                 // receive parameter values, translate it to a Message and send to
                 // send_loop
                 thread::spawn(move || param_as_message_to_sendloop(&mut sender, rx_hostin));
+
                 // send to browser
                 // thread::spawn(move || send_loop(&mut sender, rx_wsout));
 
@@ -149,6 +106,42 @@ impl yassyui {
             _ => println!("error"),
         };
     }
+}
+
+fn client_split(s: TcpStream) -> (client::Sender<WebSocketStream>, client::Receiver<WebSocketStream>) {
+    let tcpstream = s;
+    let wsstream = WebSocketStream::Tcp(tcpstream);
+    pub struct Connection<R: Read, W: Write>(R, W);
+    let connection = Connection(wsstream.try_clone().unwrap(),
+                                wsstream.try_clone().unwrap());
+
+    let request = Request::read(connection.0, connection.1).unwrap();
+    let headers = request.headers.clone(); // Keep the headers so we can check them
+
+    request.validate().unwrap(); // Validate the request
+
+    let mut response = request.accept(); // Form a response
+
+    if let Some(&WebSocketProtocol(ref protocols)) = headers.get() {
+        if protocols.contains(&("rust-websocket".to_string())) {
+            // We have a protocol we want to use
+            response.headers
+                .set(WebSocketProtocol(vec!["rust-websocket".to_string()]));
+        }
+    }
+    let mut client = response.send().unwrap(); // Send the response
+
+    let ip = client.get_mut_sender()
+        .get_mut()
+        .peer_addr()
+        .unwrap();
+
+    println!("Connection from {}", ip);
+
+    let message: Message = Message::text("Hello".to_string());
+    client.send_message(&message).unwrap();
+
+    client.split()
 }
 
 fn param_as_message_to_sendloop(txws: &mut client::Sender<WebSocketStream>, rx: mpsc::Receiver<Param>) {
@@ -178,37 +171,6 @@ fn param_as_message_to_sendloop(txws: &mut client::Sender<WebSocketStream>, rx: 
     }
 }
 
-// fn send_loop(txws: &mut client::Sender<WebSocketStream>, rx: mpsc::Receiver<Message>) {
-//     loop {
-//         // Send loop
-//         println!("send loop");
-//         let message: Message = match rx.recv() {
-//             Ok(m) => m,
-//             Err(e) => {
-//                 println!("Send Loop: {:?}", e);
-//                 return;
-//             }
-//         };
-//         match message.opcode {
-//             Type::Close => {
-//                 // If it's a close message, just send it and then return.
-//                 let _ = txws.send_message(&message);
-//                 return;
-//             }
-//             _ => (),
-//         }
-//         // Send the message
-//         match txws.send_message(&message) {
-//             Ok(()) => (),
-//             Err(e) => {
-//                 println!("Send Loop: {:?}", e);
-//                 let _ = txws.send_message(&Message::close());
-//                 return;
-//             }
-//         }
-//     }
-// }
-
 // Receive from browser
 fn receive_loop(rxws: &mut client::Receiver<WebSocketStream>,
                 write_function: lv2::LV2UIWriteFunction,
@@ -217,40 +179,16 @@ fn receive_loop(rxws: &mut client::Receiver<WebSocketStream>,
     for message in rxws.incoming_messages() {
 
         let message: Message = message.unwrap();
-
-        // match message.opcode {
-        //     // TODO: do the right thing here
-        //     Type::Close => {
-        //         // Got a close message, so send a close message and return
-        //         let _ = tx.send(Message::close());
-        //         return;
-        //     }
-        //     Type::Ping => {
-        //         match tx.send(Message::pong(message.payload)) {
-        //             // Send a pong in response
-        //             Ok(()) => (),
-        //             Err(e) => {
-        //                 println!("Receive Loop: {:?}", e);
-        //                 return;
-        //             }
-        //         }
-        //     }
-            // // Say what we received
-            // _ => {
-                let vecu8 = message.payload.into_owned();
-                let mess = String::from_utf8(vecu8).unwrap();
-                println!("message: {}", mess);
-                let res = json::decode(&mess);
-                // let myfloat = mess.parse::<f32>();
-                // println!("Receive Loop: {:?}", myfloat);
-                match res {
-                    Ok(param) => {
-                        on_ws_receive(write_function, ctrl, &param);
-                    }
-                    Err(err) => println!("Err: {}", err),
-                }
-            // }
-    // }
+        let vecu8 = message.payload.into_owned();
+        let mess = String::from_utf8(vecu8).unwrap();
+        println!("message: {}", mess);
+        let res = json::decode(&mess);
+        match res {
+            Ok(param) => {
+                on_ws_receive(write_function, ctrl, &param);
+            }
+            Err(err) => println!("Err: {}", err),
+        }
     }
 }
 
