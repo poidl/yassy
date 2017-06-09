69f4109f2c45e3ddbb78d65eb45960b67f2a597d
Stefan <hoitaus@gmail.com>
2017-06-09 14:46:06 +1000

    use slices, not pointers

--- midi/src/lib.rs
+++ midi/src/lib.rs
@@ -1,4 +1,4 @@
-pub type MidiMessage<'a> = &'a u8;
+pub type MidiMessage<'a> = &'a [u8;3];
 
 pub enum CcKind {
     ChannelVolume,
@@ -8,69 +8,55 @@ pub enum CcKind {
 pub trait MidiTranslate {
     fn noteon(&self) -> bool;
     fn noteoff(&self) -> bool;
-    fn cc(&self) -> bool;
-    fn cc_type(&self) -> CcKind;
-    fn cc_value(&self) -> f32;
 
     fn f0(&self) -> f32;
+    fn note_number(&self) -> u8;
     fn vel(&self) -> f32;
 
-    fn ccnr(&self) -> u8;
-    fn ccval(&self) -> u8;
+    // fn cc(&self) -> bool;
+    // fn cc_type(&self) -> CcKind;
+    // fn cc_value(&self) -> f32;
+    // fn ccnr(&self) -> u8;
+    // fn ccval(&self) -> u8;
 }
 
 impl<'a> MidiTranslate for MidiMessage<'a> {
     fn noteon(&self) -> bool {
-        *self & 0xf0 == 0x90
+        self[0] & 0xf0 == 0x90
     }
     fn noteoff(&self) -> bool {
-        *self & 0xf0 == 0x80
-    }
-    fn cc(&self) -> bool {
-        *self & 0xf0 == 0xb0
-    }
-    fn cc_type(&self) -> CcKind {
-        let msg = *self as *const u8;
-        unsafe{
-            let x = *msg.offset(1);
-            match x {
-                0x07 => return CcKind::ChannelVolume,
-                _    => return CcKind::Unknown
-            }
-        }
-    }
-    fn cc_value(&self) -> f32 {
-        let msg = *self as *const u8;
-        unsafe{
-            let x = *msg.offset(2);
-            (x as f32)/127f32
-        }
+        self[0] & 0xf0 == 0x80
     }
     fn f0(&self) -> f32 {
-        let msg = *self as *const u8;
-        unsafe{
-            let i = *msg.offset(1);
-            let f0 = (2.0f32.powf((((i as i8)-57) as f32)/12.0))*220.0;
-            return f0
-        }
+        let i = self[1];
+        let f0 = (2.0f32.powf((((i as i8)-57) as f32)/12.0))*220.0;
+        return f0
+    }
+    fn note_number(&self) -> u8 {
+        self[1]
     }
     fn vel(&self) -> f32 {
-        let msg = *self as *const u8;
-        unsafe{
-            let i = *msg.offset(2);
-            return i as f32 / 127 as f32
-        }
+        let i = self[2];
+        return i as f32 / 127 as f32
     }
-    fn ccnr(&self) -> u8 {
-        let msg = *self as *const u8;
-        unsafe{
-            *msg.offset(1)
-        }
-    }
-    fn ccval(&self) -> u8 {
-        let msg = *self as *const u8;
-        unsafe{
-            *msg.offset(2)
-        }
-    }
+    // fn cc(&self) -> bool {
+    //     self[0] & 0xf0 == 0xb0
+    // }
+    // fn cc_type(&self) -> CcKind {
+    //     let x = self[1];
+    //     match x {
+    //         0x07 => return CcKind::ChannelVolume,
+    //         _    => return CcKind::Unknown
+    //     }
+    // }
+    // fn cc_value(&self) -> f32 {
+    //         let x = self[2];
+    //         (x as f32)/127f32
+    // }
+    // fn ccnr(&self) -> u8 {
+    //     self[1]
+    // }
+    // fn ccval(&self) -> u8 {
+    //     self[2]
+    // }
 }
