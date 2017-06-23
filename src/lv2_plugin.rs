5efa1e14148ceca0f4ff0a8b30509a3e7a8d1643
Stefan <hoitaus@gmail.com>
2017-06-23 20:11:28 +1000

    observables

--- src/lv2_plugin.rs
+++ src/lv2_plugin.rs
@@ -2,10 +2,13 @@ extern crate libc;
 extern crate lv2;
 use std::ffi::CString;
 use std::ptr;
-use plugin;
+// use plugin;
 use midi;
 // use midi::*;
 use std::str;
+use observer;
+use observer::*;
+use types;
 
 pub struct Synthuris {
     pub midi_event: lv2::LV2Urid,
@@ -22,24 +25,32 @@ impl Synthuris {
 }
 
 #[repr(C)]
-pub struct Lv2SynthPlugin {
+pub struct Lv2SynthPlugin<'a> {
     pub map: *mut lv2::LV2UridMap,
     pub in_port: *const lv2::LV2AtomSequence,
     pub output: *mut f32,
     pub in_port_time: *const lv2::LV2AtomSequence,
     pub uris: Synthuris,
-    pub plugin: plugin::SynthPlugin,
+    // pub plugin: plugin::SynthPlugin<'a>,
+    pub in_port_synth: *mut f32,
+    pub bufferpos: Observable<'a, u32>,
+    pub midiMessage: Observable<'a, midi::MidiMessage>,
+    pub fs: Observable<'a, types::fs>,
 }
 
-impl Lv2SynthPlugin {
-    pub fn new() -> Lv2SynthPlugin {
+impl<'a> Lv2SynthPlugin<'a> {
+    pub fn new() -> Lv2SynthPlugin<'a> {
         let lv2plugin = Lv2SynthPlugin {
             map: ptr::null_mut(),
             in_port: ptr::null(),
             output: ptr::null_mut(),
             in_port_time: ptr::null(),
             uris: Synthuris::new(),
-            plugin: plugin::SynthPlugin::new(),
+            // plugin: plugin::SynthPlugin::new(),
+            in_port_synth: ptr::null_mut(), 
+            bufferpos: Observable::new(0u32),        
+            midiMessage: Observable::new([0u8,0u8,0u8]),
+            fs: Observable::new(types::fs(0f64)),
         };
         lv2plugin
     }
@@ -48,8 +59,8 @@ impl Lv2SynthPlugin {
             let midievent = self.uris.midi_event;
             let sequence_iterator = (*self.in_port).into_iter();
 
-            // connect plugin
-            self.plugin.audio_out = self.output;
+            // // connect plugin
+            // self.plugin.audio_out = self.output;
 
             // filter midi events from atom sequence
             let midievent_iterator = sequence_iterator
@@ -60,12 +71,12 @@ impl Lv2SynthPlugin {
                 .map(|x| {
                     (
                         (*x).time_in_frames as u32,
-                        &*((x as *const lv2::LV2AtomEvent).offset(1) as *const [u8;3]) as midi::MidiMessage
+                        *((x as *const lv2::LV2AtomEvent).offset(1) as *const [u8;3]) as midi::MidiMessage
                     )
                 });
             
             // dispatch to plugin
-            self.plugin.update(ievent_midimessage, n_samples);
+            self.update(ievent_midimessage, n_samples);
 
 
             // let timepositionevent = self.uris.time_position_event;
@@ -78,6 +89,34 @@ impl Lv2SynthPlugin {
             // }
             
 
+        }
+    }
+    pub fn update<T>(&mut self, iter: T, n_samples: u32) 
+    where T: Iterator<Item=(u32, midi::MidiMessage)> {
+        unsafe {
+            let mut i = 0;
+            for (ievent, mm) in iter {
+                println!("");
+                println!("Processing MIDI...");
+                while i < ievent {
+                    // let amp = self.get_amp();
+                    self.bufferpos.update(i);
+                    *self.output.offset(i as isize) = *self.in_port_synth;
+                    i =  i+1;
+                }
+                // self.notifyevent_midi(mm);
+                self.midiMessage.update(mm);
+            }
+            while i < n_samples {
+                // println!{"********** i : {}", i}
+                self.bufferpos.update(i);
+                // let amp = self.get_amp();
+                // println!(" *self.in_port_synth: {}", *self.in_port_synth);
+                // println!(" *self.output.offset(i as isize): {}", *self.output.offset(i as isize));
+                *self.output.offset(i as isize) = *self.in_port_synth;
+                // println!{"********* DEAD **********"}
+                i =  i+1;
+            }
         }
     }
     pub fn seturis(&mut self) {
@@ -97,28 +136,29 @@ impl Lv2SynthPlugin {
             0 => self.in_port = data as *const lv2::LV2AtomSequence,
             1 => self.output = data as *mut f32,
             2 => self.in_port_time = data as *const lv2::LV2AtomSequence,
-            _ => self.map_params(port, data),
+            // _ => self.map_params(port, data),
+            _ => {},
         }
     }
-    pub fn set_fs(&mut self, fs: f64) {
-        self.plugin.set_fs(fs);
-    }
-    pub fn get_amp(&mut self) -> f32 {
-        self.plugin.get_amp()
-    }
-    fn map_params(&mut self, port: u32, data: *mut libc::c_void) {
+    // pub fn set_fs(&mut self, fs: f64) {
+    //     self.plugin.set_fs(fs);
+    // }
+    // pub fn get_amp(&mut self) -> f32 {
+    //     self.plugin.get_amp()
+    // }
+    // fn map_params(&mut self, port: u32, data: *mut libc::c_void) {
 
-        let nparams = self.plugin.params.len();
-        let iport = port - 3; //TODO: don't hardcode number of input/output ports
-        if iport <= nparams as u32 - 1 {
-            println!("connecting port: {}", port);
-            self.plugin.params[iport as usize] = data as *mut f32;
-            // println!("param: {}",  *(self.synth.params[0]));
-        } else {
-            panic!("Not a valid PortIndex: {}", iport)
-        }
-    }
-    pub fn cleanup(&mut self) {
-        self.plugin.cleanup();
-    }
+    //     let nparams = self.plugin.params.len();
+    //     let iport = port - 3; //TODO: don't hardcode number of input/output ports
+    //     if iport <= nparams as u32 - 1 {
+    //         println!("connecting port: {}", port);
+    //         // self.plugin.params[iport as usize] = data as *mut f32;
+    //         // println!("param: {}",  *(self.synth.params[0]));
+    //     } else {
+    //         panic!("Not a valid PortIndex: {}", iport)
+    //     }
+    // }
+    // pub fn cleanup(&mut self) {
+    //     self.plugin.cleanup();
+    // }
 }
--- src/observer.rs
+++ src/observer.rs
@@ -1,16 +1,60 @@
 use midi;
 use midi::*;
 use std::iter::*;
+use std::rc::Rc;
+use std::marker::Copy;
 
 pub trait NoteEventObserver {
     fn next(&mut self, f0: f32, vel: f32);
 }
 
-pub type Observers = Vec<Box<Observer>>;
+pub type Observers<'a, T> = Vec<&'a mut Observer<T>>;
 
-pub struct Observable<T> {
+pub trait Observer<T>:  {
+    fn next(&mut self, mm: T);
+    // fn next_noteon(&mut self, f0: f32, vel: f32);
+    // fn next_noteoff(&mut self);
+    // fn next_fs(&mut self, fs: f64);
+    // fn next_blit(&mut self, blit: bool);
+    // fn next_postfilter(&mut self, pf: bool);
+    // fn get_amp(&mut self) -> f32;
+    // fn cleanup(&mut self);
+}
+
+// pub trait ObserverTmp  {
+//     fn next_noteoff(&mut self);
+//     fn next_fs(&mut self, fs: f64);
+//     fn next_blit(&mut self, blit: bool);
+//     fn next_postfilter(&mut self, pf: bool);
+//     fn get_amp(&mut self) -> f32;
+//     fn cleanup(&mut self);
+// }
+
+pub struct Observable<'a, T: 'a> {
     item: T,
-    observers: Observers,
+    pub observers: Observers<'a, T>,
+}
+
+impl<'a, T: Copy> Observable<'a, T> {
+    pub fn new(item: T) -> Observable<'a, T> {
+        Observable {
+            item: item,
+            observers: Vec::with_capacity(1)
+        }
+    }
+    pub fn update(&mut self, item: T) {
+        self.item = item;
+        self.notify();
+    }
+    pub fn notify(&mut self) {
+        // println!("notifying:::::::::::::::::::::::::;");
+        for o in self.observers.iter_mut() {
+        // println!(" notifying::::::::");
+            o.next(self.item);
+        // println!("::::::::");
+        }
+
+    }
 }
 
 // impl Observers {
@@ -19,18 +63,9 @@ pub struct Observable<T> {
 //     }
 // }
 
-pub trait Observer:  {
-    fn next(&mut self, mm: MidiMessage);
-    fn next_noteon(&mut self, f0: f32, vel: f32);
-    fn next_noteoff(&mut self);
-    fn next_fs(&mut self, fs: f64);
-    fn next_blit(&mut self, blit: bool);
-    fn next_postfilter(&mut self, pf: bool);
-    fn get_amp(&mut self) -> f32;
-    fn cleanup(&mut self);
-}
 
-pub trait ObservableTrait {
+pub trait ObservableTrait<T> {
+    fn notify(&mut self, item: T) {}
     fn notifyevent_noteon(&mut self, f0: f32, vel: f32) {}
     fn notifyevent_noteoff(&mut self) {}
     fn notifyevent_fs(&mut self, fs: f64) {}
@@ -38,33 +73,39 @@ pub trait ObservableTrait {
     fn notifyevent_postfilter(&mut self, pf: bool) {}
 }
 
-impl<T> ObservableTrait for Observable<T> {
-    fn notifyevent_noteon(&mut self, f0: f32, vel: f32) {
-        for o in &mut self.observers {
-            o.next_noteon(f0, vel)
-        }
-    }
-    fn notifyevent_noteoff(&mut self) {
-        for o in &mut self.observers {
-            o.next_noteoff()
-        }
-    }
-    fn notifyevent_fs(&mut self, fs: f64) {
-        for o in &mut self.observers {
-            o.next_fs(fs)
-        }
-    }
-    fn notifyevent_blit(&mut self, blit: bool) {
-        for o in &mut self.observers {
-            o.next_blit(blit)
-        }
-    }
-    fn notifyevent_postfilter(&mut self, pf: bool) {
-        for o in &mut self.observers {
-            o.next_postfilter(pf)
-        }
-    }
-}
+
+// impl<T> ObservableTrait<T> for Observable<T> where T:'a {
+//     fn notify(&mut self, item: T) {
+//         for o in &mut self.observers {
+//             o.next(item)
+//         }        
+//     }
+//     fn notifyevent_noteon(&mut self, f0: f32, vel: f32) {
+//         for o in &mut self.observers {
+//             o.next_noteon(f0, vel)
+//         }
+//     }
+//     fn notifyevent_noteoff(&mut self) {
+//         for o in &mut self.observers {
+//             o.next_noteoff()
+//         }
+//     }
+//     fn notifyevent_fs(&mut self, fs: f64) {
+//         for o in &mut self.observers {
+//             o.next_fs(fs)
+//         }
+//     }
+//     fn notifyevent_blit(&mut self, blit: bool) {
+//         for o in &mut self.observers {
+//             o.next_blit(blit)
+//         }
+//     }
+//     fn notifyevent_postfilter(&mut self, pf: bool) {
+//         for o in &mut self.observers {
+//             o.next_postfilter(pf)
+//         }
+//     }
+// }
 
 // fn next(&mut self, item: T) {
 //     self.on = true;
